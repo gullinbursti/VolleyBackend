ba2ef0dcf3ee7f302869823922000946ce98af2e
Shane Hill <shanehill00@gmail.com>
2013-10-31 17:36:40 +0000

    making deployment of multiple versions easier

--- index.php
+++ index.php
@@ -4,14 +4,8 @@ $r = new BIM_Controller;
 $r->handleReq();
 
 function bootstrap(){
-    $releaseId = '';
-    $matches = array();
-    preg_match( '@^/api/(.+?)/@', $_SERVER['SCRIPT_URL'], $matches );
-    if( !empty($matches[1]) ){
-        $releaseId = $matches[1];
-        $incPath = ".:/usr/share/php:/usr/share/pear:/home/volley/deployed/$releaseId/classes:/home/volley/deployed/$releaseId/lib:/home/volley/deployed/$releaseId/lib/smtp_mailer_swift/lib/classes";
-        set_include_path($incPath);
-        define('RELEASE_ID_KEY_PREFIX',$releaseId);
-    }
+    $baseDir = __DIR__;
+    $incPath = ".:/usr/share/php:/usr/share/pear:$baseDir/classes:$baseDir/lib:$baseDir/lib/smtp_mailer_swift/lib/classes";
+    set_include_path($incPath);
     require_once 'vendor/autoload.php';
 }
