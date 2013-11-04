79fc5d69650dd5e3a1cb928a11b80ea63b17260c
Shane Hill <shanehill00@gmail.com>
2013-11-04 16:08:16 +0000

    adding an index page to the admin stuff

---
+++ bin/admin/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+<a target='_blank' href="carlos_danger.php">Carlos Danger</a><br>
+<a target='_blank' href="create_volley.php">Create a Volley for Team Volley</a><br>
+<a target='_blank' href="manage_user.php">Edit a User</a><br>
+<a target='_blank' href="edit_invite_messages.php">Edit Invite Messages</a><br>
+<a target='_blank' href="manage_explore.php">Manage Explore Section</a><br>
+<a target='_blank' href="wipe_user.php">Wipe a User</a><br>
+<a target='_blank' href="edit_tags.php">Edit Tags</a><br>
+</body>
+</html>
--- bin/admin/manage_user.php
+++ bin/admin/manage_user.php
@@ -1,5 +1,3 @@
 <?php
-$baseDir = '/home/volley/admin';
-set_include_path(".:/usr/share/php:/usr/share/pear:$baseDir/classes:$baseDir/lib:$baseDir/lib/smtp_mailer_swift/lib/classes");
 require_once 'vendor/autoload.php';
 BIM_App_Admin::manageUser();
