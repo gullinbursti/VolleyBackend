618b5b199ff2ab4a744327607e7aade219c41d83
Shane Hill <shanehill00@gmail.com>
2013-11-04 17:35:32 +0000

    fix to the admin index page

--- bin/admin/index.php
+++ bin/admin/index.php
@@ -1,20 +1,30 @@
 <html>
 <head>
+    <script src='http://code.jquery.com/jquery-1.10.1.min.js'></script>
 </head>
 <body>
-<a target='_blank' href="carlos_danger.php">Carlos Danger</a><br>
-<a target='_blank' href="create_volley.php">Create a Volley for Team Volley</a><br>
-<a target='_blank' href="manage_user.php">Edit a User</a><br>
-<a target='_blank' href="edit_invite_messages.php">Edit Invite Messages</a><br>
-<a target='_blank' href="manage_explore.php">Manage Explore Section</a><br>
-<a target='_blank' href="wipe_user.php">Wipe a User</a><br>
-<a target='_blank' href="edit_tags.php">Edit Tags</a><br>
+<a href="carlos_danger.php">Carlos Danger</a><br>
+<a href="create_volley.php">Create a Volley for Team Volley</a><br>
+<a href="manage_user.php">Edit a User</a><br>
+<a href="edit_invite_messages.php">Edit Invite Messages</a><br>
+<a href="manage_explore.php">Manage Explore Section</a><br>
+<a href="wipe_user.php">Wipe a User</a><br>
+<a href="edit_tags.php">Edit Tags</a><br>
+<br><hr><br>
 <?php 
-//require_once 'vendor/autoload.php';
-//$names = BIM_Config::getBootConfNames();
-//foreach( $names as $name ){
-//    echo "<a target='_blank' href='boot/edit/$name'>Edit Boot Conf $name</a><br>";
-//}
+require_once 'vendor/autoload.php';
+$names = BIM_Config::getBootConfNames();
+foreach( $names as $name ){
+    echo "<a href='boot/edit/$name'>Edit Boot Conf $name</a><br>";
+}
 ?>
+
+<script type='text/javascript'>
+function createNewBootConf(){
+	var name = $("#bootName").val();
+    location.href='boot/edit/' + name;
+}
+</script>
+<input type='text' id='bootName'><input type='button' value='create new boot conf' onclick='createNewBootConf();'>
 </body>
 </html>
