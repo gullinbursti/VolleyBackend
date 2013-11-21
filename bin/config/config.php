454313b598eb9174734ed1deed8947cda490b176
Shane Hill <shanehill00@gmail.com>
2013-11-21 21:45:59 +0000

    adding config logic

--- bin/config/config.php
+++ bin/config/config.php
@@ -1,4 +1,7 @@
 <?php
+$baseDir = dirname( dirname( __DIR__ ) );
+set_include_path(".:/usr/share/php:/usr/share/pear:$baseDir/classes:$baseDir/lib:$baseDir/lib/smtp_mailer_swift/lib/classes");
+
 require_once 'vendor/autoload.php';
 $requestPath = isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : '';
 $build = 'live';
