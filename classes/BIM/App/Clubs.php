a354c72982646287cd384af0d7b01db1e34d834e
Shane Hill <shanehill00@gmail.com>
2014-01-29 22:18:47 +0000

    adding in the club messaging

--- classes/BIM/App/Clubs.php
+++ classes/BIM/App/Clubs.php
@@ -29,12 +29,17 @@ class BIM_App_Clubs extends BIM_App_Base{
         if( !is_array( $numbers ) ){
             $numbers = array( $numbers );
         }
+        
+        $clubOwner = BIM_Model_User::get( $ownerId );
+        
         foreach( $numbers as $number ){
             $client = BIM_Utils::getTwilioClient();
             $conf = BIM_Config::twilio();
             $number = preg_replace('/\.\s\-\+/', '', $number);
             $number = "+$number";
             $msg = BIM_Config_Dynamic::clubSmsInviteMsg();
+            $msg = str_replace('[CLUBNAME]',$clubName, $msg);
+            $msg = str_replace('[USERNAME]',$clubOwner->username, $msg);
             $sms = $client->account->sms_messages->create( $conf->api->number, $number, $msg );
         }
     }
@@ -42,9 +47,14 @@ class BIM_App_Clubs extends BIM_App_Base{
     public static function emailInvites($addys, $clubName, $ownerId){
         $emailData = BIM_Config::clubEmailInvite();
         $e = new BIM_Email_Swift( BIM_Config::smtp() );
+        
+        $clubOwner = BIM_Model_User::get( $ownerId );
+        
         foreach( $addys as $addy ){
             $emailData->to_email = $addy;
-            $emailData->text = 'foo goo foo goo';
+            $msg = $emailData->text;
+            $msg = str_replace('[CLUBNAME]',$clubName, $msg);
+            $msg = str_replace('[USERNAME]',$clubOwner->username, $msg);
             $e->sendEmail( $emailData );
         }
     }
