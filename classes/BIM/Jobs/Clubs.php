12b2beda8ea745b57b12aa4df6c8f4a6f4642459
Shane Hill <shanehill00@gmail.com>
2014-02-12 01:40:50 +0000

    removing unnecessary json encoding that was causing the workers to throw an error

--- classes/BIM/Jobs/Clubs.php
+++ classes/BIM/Jobs/Clubs.php
@@ -2,11 +2,7 @@
 
 class BIM_Jobs_Clubs extends BIM_Jobs{
     
-    /*
-     * SUBMIT MATCHING CHALLENGE JOBS
-     */
     public static function queueNotifyInvitees( $clubId, $users, $nonUsers ){
-        $users = json_encode( $users );
         $job = array(
         	'class' => __CLASS__,
         	'method' => 'notifyInvitees',
