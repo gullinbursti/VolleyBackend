74feb5e8e515b4182c7916b3652ef2727ea4e31a
Shane Hill <shanehill00@gmail.com>
2014-02-12 20:57:29 +0000

    fixes to elastic search backup script

--- bin/elasticsearch/backup.sh
+++ bin/elasticsearch/backup.sh
@@ -3,7 +3,7 @@ TO_FOLDER=/home/shane/esbackups
 FROM=/elasticsearch/data
  
 DATE=`date +%Y-%m-%d_%H`
-TO=$TO_FOLDER/$DATE/
+TO=$TO_FOLDER/$DATE
 echo "rsync from $FROM to $TO"
 # the first times rsync can take a bit long - do not disable flusing
 rsync -a $FROM $TO
@@ -16,3 +16,8 @@ rsync -a $FROM $TO
  
 $SCRIPTS/es-flush-disable.sh false
 
+# now zip up final product
+echo "\nzipping $TO $TO.tar.gz "
+cd $TO_FOLDER
+/bin/tar zcf $DATE.tar.gz $DATE
+/bin/rm -rf $DATE
