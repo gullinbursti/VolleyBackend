bc907a58286a7975f6a1ac7bc4fe96bb5d2024e4
Pedro <digitalrounin@gmail.com>
2014-05-20 15:22:56 -0700

    Curl for uploading challange

---
+++ curl/challenges_create.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -v --request POST "http://api-dev.letsvolley.com/api/pedro/challenges/create" \
+     --data-urlencode "challengeID=0" \
+     --data-urlencode "clubID=0" \
+     --data-urlencode "imgURL=https://hotornot-challenges.s3.amazonaws.com/86793eee81144ca9ae32c4e7544457a6-bcafe25a99b64c8db308cbe77b07854e_1400615027" \
+     --data-urlencode "subject=" \
+     --data-urlencode "targets=" \
+     --data-urlencode "userID=131820"
+
