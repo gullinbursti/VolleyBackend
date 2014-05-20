5c1b631493fe1e69b74dd852a11982b763b7128f
Pedro H <pedro@builtinmenlo.com>
2014-05-20 20:34:55 +0000

    Updated curl command

--- curl/users_getclubs.sh
+++ curl/users_getclubs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+    #"http://api-dev.letsvolley.com/api/pedro/users/getclubs" \
+
 curl -v --request POST \
     "http://api-dev.letsvolley.com/api/pedro/users/getclubs" \
-    --data-urlencode "userID=131820"
+    --data-urlencode "userID=131826" \
+    --data-urlencode "sort=1"
 
