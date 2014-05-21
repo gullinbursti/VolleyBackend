1de10fe0642dc95760f04434698d7961b90a3835
Pedro H <pedro@builtinmenlo.com>
2014-05-21 20:23:18 +0000

    Use and ID that actually works

--- curl/users_getclubs.sh
+++ curl/users_getclubs.sh
@@ -4,6 +4,6 @@
 
 curl -v --request POST \
     "http://api-dev.letsvolley.com/api/pedro/users/getclubs" \
-    --data-urlencode "userID=131826" \
+    --data-urlencode "userID=131820" \
     --data-urlencode "sort=1"
 
