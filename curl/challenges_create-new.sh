bb64adeb9e982a9f3728170799ea5a4ad590b292
Pedro H <pedro@builtinmenlo.com>
2014-05-23 23:06:58 +0000

    Better subjects for the kingdom!

--- curl/challenges_create-new.sh
+++ curl/challenges_create-new.sh
@@ -5,6 +5,7 @@ curl -v --request POST "http://api-dev.letsvolley.com/api/pedro/challenges/creat
      --data-urlencode "clubID=0" \
      --data-urlencode "imgURL=https://hotornot-challenges.s3.amazonaws.com/86793eee81144ca9ae32c4e7544457a6-bcafe25a99b64c8db308cbe77b07854e_1400615027" \
      --data-urlencode "subject=#bestFriend" \
+     --data-urlencode "subjects=[ \"#bestFail\", \"#funnyFace\" ]" \
      --data-urlencode "targets=" \
      --data-urlencode "userID=131820"
 
