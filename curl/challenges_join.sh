06091cd56e448380568efc8858548a900fd6aeae
Pedro <digitalrounin@gmail.com>
2014-05-24 19:53:20 -0700

    Vary the imgURL a bit

--- curl/challenges_join.sh
+++ curl/challenges_join.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+epoch=$(date +%s)
+
 curl -v --request POST "http://api-dev.letsvolley.com/api/pedro/challenges/join" \
-     --data-urlencode "imgURL=https://hotornot-challenges.s3.amazonaws.com/86793eee81144ca9ae32c4e7544457a6-bcafe25a99b64c8db308cbe77b07854e_1400615027" \
+     --data-urlencode "imgURL=https://hotornot-challenges.s3.amazonaws.com/86793eee81144ca9ae32c4e7544457a6-bcafe25a99b64c8db308cbe77b07854e_${epoch}" \
      --data-urlencode "userID=2466" \
      --data-urlencode "challengeID=252973" \
      --data-urlencode "subject=#bangbang" 
