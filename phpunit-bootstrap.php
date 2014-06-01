6bf1516818c855c94f7206b44b05202b69e93bc3
Pedro H <pedro@builtinmenlo.com>
2014-06-01 21:23:55 +0000

    Adding Hamcrest to path

--- phpunit-bootstrap.php
+++ phpunit-bootstrap.php
@@ -1,4 +1,10 @@
 <?php
 $baseDir = __DIR__;
-set_include_path(".:/usr/share/php:/usr/share/pear:$baseDir/classes:$baseDir/lib:$baseDir/lib/smtp_mailer_swift/lib/classes");
+
+$prod_path = ".:/usr/share/php:/usr/share/pear:$baseDir/classes:$baseDir/lib:$baseDir/lib/smtp_mailer_swift/lib/classes";
+$dev_path = "$baseDir/vendor/hamcrest/hamcrest-php/hamcrest";
+$final_path = "$prod_path:$dev_path";
+
+set_include_path($final_path);
+
 require_once 'lib/vendor/autoload.php';
