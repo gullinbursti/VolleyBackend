e6c6c8801e5ba884395a3d4cb9408f731d8af324
Pedro H <pedro@builtinmenlo.com>
2014-06-05 01:28:15 +0000

    Making Nexmo call!!!!  Yay!!!

--- classes/BIM/Integration/Nexmo/TwoFactorAuth.php
+++ classes/BIM/Integration/Nexmo/TwoFactorAuth.php
@@ -9,9 +9,54 @@ class BIM_Integration_Nexmo_TwoFactorAuth {
 
     public function sendPin( $phone, $pin )
     {
-        $apiKey = $this->_config->apiKey;
-        $apiSecret = $this->_config->apiSecret;
+        // Prepare for call
         $endPoint = $this->_config->twoFactorJsonEndpoint;
+        $query = array(
+            'api_key' => $this->_config->apiKey,
+            'api_secret' => $this->_config->apiSecret,
+            'to' => "$phone",
+            'pin' => "$pin"
+        );
+        $queryString = http_build_query( $query );
+        $curlOptions = array(
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $queryString,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => false,
+            CURLOPT_VERBOSE => false,
+            CURLOPT_FAILONERROR => true,
+        );
+
+        // Make web-service call
+        $handle = curl_init( $endPoint );
+        curl_setopt_array( $handle, $curlOptions );
+        $curlResponse = curl_exec($handle);
+        $curlErrorNumber = curl_errno( $handle );
+        $curlError = curl_error( $handle );
+        curl_close($handle);
+
+        // Post processing
+        $nexmoSuccessful = $this->nexmoCallSuccessful( $curlResponse );
+
+        // TODO - Add logging on error
+
+        // Return true IFF curl error num is 0, and nexmoSuccessful is true
+        return ($curlErrorNumber == 0) && $nexmoSuccessful;
+    }
+
+    protected function nexmoCallSuccessful( $jsonString ) {
+        $json = json_decode( $jsonString );
+
+        if ( $json
+            && $json->{'message-count'} == 1
+            && $json->messages[0]->status == 0
+        ) {
+            $successful = true;
+        } else {
+            $successful = false;
+        }
+
+        return $successful;
     }
 }
 
