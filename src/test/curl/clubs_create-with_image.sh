a4f023c705277441289ef34ecba5ebe4baf3d501
Pedro H <pedro@builtinmenlo.com>
2014-06-11 22:11:32 +0000

    Renamed imgURL to imageURL

--- src/test/curl/clubs_create-with_image.sh
+++ src/test/curl/clubs_create-with_image.sh
@@ -7,5 +7,5 @@ curl "http://api-dev.letsvolley.com/api/pedro/clubs/create" \
     --data-urlencode "userID=133703" \
     --data-urlencode "name=TEST_CLUB_${time_stamp}" \
     --data-urlencode "description=Test club ${time_stamp}" \
-    --data-urlencode "imgURL=0007ab92a1df4d27a91670f9ac9c1ca3_43850f0426e04ca5826060f44163183f-1387815886" \
+    --data-urlencode "imageURL=0007ab92a1df4d27a91670f9ac9c1ca3_43850f0426e04ca5826060f44163183f-1387815886" \
 
