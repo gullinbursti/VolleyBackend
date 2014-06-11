5cb5b22d7daa0f28a970cf032283bd0d6660061b
Pedro H <pedro@builtinmenlo.com>
2014-06-11 22:08:00 +0000

    Script to create a club with an image

---
+++ src/test/curl/clubs_create-with_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+time_stamp=$(date +%Y%m%d\.%H%M%S)
+
+curl "http://api-dev.letsvolley.com/api/pedro/clubs/create" \
+    --request POST \
+    --data-urlencode "userID=133703" \
+    --data-urlencode "name=TEST_CLUB_${time_stamp}" \
+    --data-urlencode "description=Test club ${time_stamp}" \
+    --data-urlencode "imgURL=0007ab92a1df4d27a91670f9ac9c1ca3_43850f0426e04ca5826060f44163183f-1387815886" \
+
