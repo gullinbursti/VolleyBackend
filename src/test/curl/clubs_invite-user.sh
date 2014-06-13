c22f6312b9064978ba042adde253b9946258c304
Pedro H <pedro@builtinmenlo.com>
2014-06-13 23:59:04 +0000

    A realy user ID

--- src/test/curl/clubs_invite-user.sh
+++ src/test/curl/clubs_invite-user.sh
@@ -3,5 +3,5 @@
 curl -v --request POST "http://api-dev.letsvolley.com/api/pedro/clubs/invite" \
     --data-urlencode "clubID=40" \
     --data-urlencode "userID=131820" \
-    --data-urlencode "users=133358" 
+    --data-urlencode "users=64846" 
 
