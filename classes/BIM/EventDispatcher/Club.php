d84fc063a9b73a9beee56d7c5dc45505d9061874
Pedro H <pedro@builtinmenlo.com>
2014-07-29 21:56:58 +0000

    Added timestamp to call, the circle is complete

--- classes/BIM/EventDispatcher/Club.php
+++ classes/BIM/EventDispatcher/Club.php
@@ -2,9 +2,10 @@
 class BIM_EventDispatcher_Club extends BIM_EventDispatcher_Base {
 
     public function invitationToMember( $clubId, $actorMemberId, $inviteeMemberId ) {
-
-        $this->dispatchEvent('club.tasks.invitation_sent', $clubId, $actorMemberId, $inviteeMemberId, 4);
-
+        $utc = new DateTimeZone("UTC");
+        $now = new DateTime( "now", $utc );
+        $nowString = $now->format(DateTime::ISO8601);
+        $this->dispatchEvent('club.tasks.invitation_sent', $clubId, $actorMemberId, $inviteeMemberId, $nowString);
     }
 
 }
