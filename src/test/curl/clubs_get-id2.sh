817b239122579f5de34e190bc13076b6f94f8ad9
Pedro <ordeph@gmail.com>
2014-08-18 23:15:39 +0000

    It works

--- src/test/curl/clubs_get-id2.sh
+++ src/test/curl/clubs_get-id2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -v --request POST \
-    "http://api-dev.letsvolley.com/api/pedro/clubs/get" \
-    --data-urlencode "clubID=2" \
-    --data-urlencode "userID=122878"
+    "http://volley-api.dev.selfieclubapp.com/sc0005/clubs/get" \
+    --data-urlencode "clubID=133" \
+    --data-urlencode "userID=130546"
 
