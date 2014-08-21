f8721ba158f2e96eea8da4ce848de874af017368
Phillip Winn <phillip@builtinmenlo.com>
2014-08-21 16:21:33 +0000

    In-process, unfinished

--- classes/BIM/EventDispatcher/Club.php
+++ classes/BIM/EventDispatcher/Club.php
@@ -8,6 +8,13 @@ class BIM_EventDispatcher_Club extends BIM_EventDispatcher_Base {
         $this->dispatchEvent('club.tasks.invitation_sent', $clubId, $actorMemberId, $inviteeMemberId, $nowString);
     }
 
+    public function invitationToNonMember( $clubId, $actorMemberId, $inviteePhoneNumber ) {
+        $utc = new DateTimeZone("UTC");
+        $now = new DateTime( "now", $utc );
+        $nowString = $now->format(DateTime::ISO8601);
+        $this->dispatchEvent('messaging.tasks.send_sms_invitation', $clubId, $actorMemberId, $inviteePhoneNumber, $nowString);
+    }
+
     public function memberJoined( $clubId, $actorMemberId) {
         $utc = new DateTimeZone("UTC");
         $now = new DateTime( "now", $utc );
