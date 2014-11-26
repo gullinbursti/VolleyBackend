f9d0578fad537f305e26d3194d961d385f3c5551
Pedro <ordeph@gmail.com>
2014-11-26 08:35:43 +0000

    Forgot to include these

--- bin/data_schemas/hotornot-dev-schema-sc0009-00.sql
+++ bin/data_schemas/hotornot-dev-schema-sc0009-00.sql
@@ -12,5 +12,11 @@ CREATE TABLE `tbl_sku` (
 INSERT INTO `tbl_sku` (`id`, `name`, `description`, `updated`, `created`)
     VALUE (1, 'selfieclub', 'The original, and default SKU', NOW(), NOW());
 
+INSERT INTO `tbl_sku` (`name`, `description`, `updated`, `created`)
+    VALUE ('emoji', 'Emoji app', NOW(), NOW());
+
+INSERT INTO `tbl_sku` (`name`, `description`, `updated`, `created`)
+    VALUE ('last24', 'Last24?', NOW(), NOW());
+
 ALTER TABLE `tblUsers` ADD COLUMN `sku_id` int(10) unsigned NOT NULL DEFAULT 1 AFTER `id`;
 ALTER TABLE `tblUsers` ADD CONSTRAINT `sku_id_fk_1` FOREIGN KEY (`sku_id`) REFERENCES `tbl_sku` (`id`);
